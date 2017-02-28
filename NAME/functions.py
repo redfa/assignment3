bf3de3a8873bfce7af4258b0982b431251af7ac9
dell-pc <dell-pc@193.1.165.18>
2017-02-28 16:00:42 +0000

    forget to write the count light function in former version

--- NAME/functions.py
+++ NAME/functions.py
@@ -20,7 +20,7 @@ def turnon(list,x1,x2,y1,y2):
             if list[m][n]==False:
                 list[m][n]=True
     return list
-#pprint(turnon(listcreate(10),0,0,1,1))
+#pprint(turnon(listcreate(10),0,9,0,9))
 def turnoff(list,x1,x2,y1,y2):
     '''turnoff funciton'''
     x1=max(x1,0)
@@ -44,7 +44,15 @@ def switch(list,x1,x2,y1,y2):
             else:
                 list[m][n]=True
     return list   
-pprint(switch(listcreate(10),1,5,1,1))    
+#pprint(switch(listcreate(10),1,5,1,1))    
+def countlight(list):
+    sum=0
+    for item in list:
+        sum+=item.count(True)
+    return sum
+pprint(turnon(listcreate(10),0,9,0,9))
+pprint(countlight(turnon(listcreate(10),0,9,0,9)))
+
     
         
     
