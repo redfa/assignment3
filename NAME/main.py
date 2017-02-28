551b82298365bc15827e55fa5aa791bb44046945
dell-pc <dell-pc@193.1.165.101>
2017-02-28 10:38:40 +0000

    successfully catch the number from file

--- NAME/main.py
+++ NAME/main.py
@@ -4,7 +4,7 @@ Created on Feb 26, 2017
 @author: Peng Xu
 '''
 import urllib.request
-''''import argparse
+'''import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', help='input help')
@@ -13,8 +13,25 @@ args = parser.parse_args()
 url = args.input'''
 # read the url 
 url="http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt" 
-data=urllib.request.urlopen(url).read()  
+data=urllib.request.urlopen(url).read()
 buffer=data.decode('UTF-8')  
-print(buffer)
+content=buffer.splitlines()
+#print(content)
+'''catch the key number and words'''
+size= int(content[0])
+list1=[]
+for line in content[1:]:
+    value=line.split()
+    #print(value)
+    
+    flag=0
+    for item in value:
+        value=[int(s) for s in item.split(',') if s.isdigit()]
+        if value:
+            list1.append(value)
+        
+print(list1)
+
+#print (size)
 
   
