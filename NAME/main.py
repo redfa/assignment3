7f3940937f35cb2460a6bbce9a7a220d05ce0712
dell-pc <dell-pc@193.1.165.18>
2017-02-28 20:02:37 +0000

    in this version, every file can run but c's answer is wrong

--- NAME/main.py
+++ NAME/main.py
@@ -5,6 +5,7 @@ Created on Feb 26, 2017
 '''
 import urllib.request
 from functions import turnoff,turnon,switch,listcreate,countlight
+import re
 '''import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,7 +14,7 @@ args = parser.parse_args()
 
 url = args.input'''
 # read the url 
-url=" http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_b.txt" 
+url=" http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_c.txt" 
 data=urllib.request.urlopen(url).read()
 buffer=data.decode('UTF-8')  
 content=buffer.splitlines()
@@ -25,30 +26,27 @@ x1=0
 x2=0
 y1=0
 y2=0
+
 for line in content[1:]:
     value=line.split()
+    print(line)
     #print(value)
-    flag=0
+    number=re.findall("[-+]?\d+[\.]?\d*[eE]?[-+]?\d*", line)
+    #print(re.findall("[-+]?\d+[\.]?\d*[eE]?[-+]?\d*", line))
 
-    for item in value:
-        number=[int(s) for s in item.split(',') if s.isdigit() or (s.startswith('-') and s[1:].isdigit())]
-        '''get the key number from file and assigned them to x1, x2, y1, y2'''        
-        if number and flag % 2==0:
-            x1=number[0]
-            y1=number[1]
-            flag=1
-        elif number and flag % 2==1:
-            x2=number[0]
-            y2=number[1]
-            flag=0
-            '''get the key word of command and exctue the right funcion'''
+    '''get the key number from file and assigned them to x1, x2, y1, y2'''    
+    x1=int(number[0])
+    y1=int(number[1])
+    x2=int(number[2])
+    y2=int(number[3])    
+    '''get the key word of command and exctue the right funcion'''
             #print(value,x1,x2,y1,y2)
-            if value[0]=='turn' and value[1]=='on':
-                turnon(list1,x1,x2,y1,y2)
-            elif value[0]=='turn' and value[1]=='off':
-                turnoff(list1,x1,x2,y1,y2)
-            elif value[0]=='switch':
-                switch(list1,x1,x2,y1,y2)
+    if value[0]=='turn' and value[1]=='on':
+        turnon(list1,x1,x2,y1,y2)
+    elif value[0]=='turn' and value[1]=='off':
+        turnoff(list1,x1,x2,y1,y2)
+    elif value[0]=='switch':
+        switch(list1,x1,x2,y1,y2)
             
 
 result=countlight(list1)
