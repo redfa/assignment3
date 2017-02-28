bb1e746a12b55281090f53b883aaf1c629af1b4b
dell-pc <dell-pc@193.1.165.18>
2017-02-28 18:27:30 +0000

    first run of the file. for file assign3 and assign3_a assign_b is right, for assign_c is wrong, assign_d can't run

--- NAME/main.py
+++ NAME/main.py
@@ -4,6 +4,7 @@ Created on Feb 26, 2017
 @author: Peng Xu
 '''
 import urllib.request
+from functions import turnoff,turnon,switch,listcreate,countlight
 '''import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,14 +13,14 @@ args = parser.parse_args()
 
 url = args.input'''
 # read the url 
-url=" http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_c.txt" 
+url=" http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_b.txt" 
 data=urllib.request.urlopen(url).read()
 buffer=data.decode('UTF-8')  
 content=buffer.splitlines()
 #print(content)
 '''catch the key number and words'''
 size= int(content[0])
-list1=[]
+list1=listcreate(size)
 x1=0
 x2=0
 y1=0
@@ -30,26 +31,31 @@ for line in content[1:]:
     flag=0
 
     for item in value:
-        list1=[]
-        value=[int(s) for s in item.split(',') if s.isdigit() or (s.startswith('-') and s[1:].isdigit())]
-        
-        if value and flag % 2==0:
-            x1=value[0]
-            y1=value[1]
+        number=[int(s) for s in item.split(',') if s.isdigit() or (s.startswith('-') and s[1:].isdigit())]
+        '''get the key number from file and assigned them to x1, x2, y1, y2'''        
+        if number and flag % 2==0:
+            x1=number[0]
+            y1=number[1]
             flag=1
-        elif value and flag % 2==1:
-            x2=value[0]
-            y2=value[1]
+        elif number and flag % 2==1:
+            x2=number[0]
+            y2=number[1]
             flag=0
-            #print(x1,x2,y1,y2)
+            '''get the key word of command and exctue the right funcion'''
+            #print(value,x1,x2,y1,y2)
+            if value[0]=='turn' and value[1]=='on':
+                turnon(list1,x1,x2,y1,y2)
+            elif value[0]=='turn' and value[1]=='off':
+                turnoff(list1,x1,x2,y1,y2)
+            elif value[0]=='switch':
+                switch(list1,x1,x2,y1,y2)
+            
+
+result=countlight(list1)
+
+print(result)
+            
+                
             
         
-    ''' create a list include all key number needed'''   
-    #x1=list1[i][0]
-    #y1=list1[i][1]
-    #x2=list1[i+1][0]
-    #y2=list1[i+1][1]
-    #i+=2
-    #print(x1,x2,y1,y2)
 
-#print (size)
