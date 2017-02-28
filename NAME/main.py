a7f076e6c659dca91c6161cd60dc0bfad18379d8
dell-pc <dell-pc@193.1.165.18>
2017-02-28 15:17:05 +0000

    find that if I use the isdigit, it can not catch the negative number so change to this version to fix this problem

--- NAME/main.py
+++ NAME/main.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 url = args.input'''
 # read the url 
-url="http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt" 
+url=" http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_c.txt" 
 data=urllib.request.urlopen(url).read()
 buffer=data.decode('UTF-8')  
 content=buffer.splitlines()
@@ -20,18 +20,36 @@ content=buffer.splitlines()
 '''catch the key number and words'''
 size= int(content[0])
 list1=[]
+x1=0
+x2=0
+y1=0
+y2=0
 for line in content[1:]:
     value=line.split()
     #print(value)
-    
     flag=0
+
     for item in value:
-        value=[int(s) for s in item.split(',') if s.isdigit()]
-        if value:
-            list1.append(value)
+        list1=[]
+        value=[int(s) for s in item.split(',') if s.isdigit() or (s.startswith('-') and s[1:].isdigit())]
         
-print(list1)
+        if value and flag % 2==0:
+            x1=value[0]
+            y1=value[1]
+            flag=1
+        elif value and flag % 2==1:
+            x2=value[0]
+            y2=value[1]
+            flag=0
+            #print(x1,x2,y1,y2)
+            
+        
+    ''' create a list include all key number needed'''   
+    #x1=list1[i][0]
+    #y1=list1[i][1]
+    #x2=list1[i+1][0]
+    #y2=list1[i+1][1]
+    #i+=2
+    #print(x1,x2,y1,y2)
 
 #print (size)
-
-  
