1a6c9af7b5ba9073312a6dec33c4b1caa098db8e
dell-pc <dell-pc@193.1.165.18>
2017-03-01 15:43:29 +0000

    NEW version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name="ledtester",
+setup(name="skeleton",
       version="0.1",
       description="LED Testing for Assignment3 in COMP30670 2017",
       url="",
-      author="Aonghus Lawlor",
+      author="Peng Xu",
       author_email="aonghus.lawlor@ucd.ie",
       licence="GPL3",
-      packages=['src'],
+      packages=['NAME'],
       entry_points={
-        'console_scripts':['led_tester=src.main:main']
+        'console_scripts':['led_tester=NAME.main:main']
         },
       install_requires=[
           'numpy',
